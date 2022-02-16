f5c037ebb26c04738790b4b47c259cb7dae28804
SviatoslavIvanchuk <isv1796@gmail.com>
2022-02-16 18:16:53 +0200

    Lesson1_ClassWork

--- app.js
+++ app.js
@@ -1,143 +1,150 @@
-// ДЗ:
-//
-//     Всі дії виконувати з допомогою модулів (вручну нічого не створюємо)
-// Створити основну папку (main), в яку покласти дві інші папки: перша - online, друга - inPerson
-// Потім створити в вашому головному файлі (для прикладу app.js) два масиви з обєктами user
-// ({. name: "Andrii", age: 22, city: "Lviv" }),  відповідно перший - onlineUsers, другий - inPersonUsers;
-// і створити файли txt в папках (online, inPerson) в яких як дату покласти юзерів з ваших масивів,
-// але щоб ваш файл виглядав як NAME: ім'я з обєкту і т.д і всі пункти з нового рядка.
-//
-// Коли ви це виконаєте напишіть функцію яка буде міняти місцями юзерів з одного файлу і папки в іншу.
-// (ті, що були в папці inPerson будуть в папці online)
-
-
 const fs = require("fs");
 const path = require("path");
 
-const onlineUser = [
-    {name: "Andrii", age: 22, city: "Lviv" },
-    {name: "Oleg", age: 24, city: "Frankivsk" },
-    {name: "Vitalik", age: 22, city: "Kyiv" },
-]
 
-const inPersonUser = [
-    {name: "Jack", age: 22, city: "Austin" },
-    {name: "Kate", age: 24, city: "NY" },
-    {name: "Alice", age: 22, city: "LA" },
-]
+// ===============================Task1========================================
+// 1. Спробуйте створити якийсь файл txt, прочитайте з нього дані і одразу, дані які ви
+// отримали запишіть їх в інший файл, в вас вийде невеликий callback hell, пізніше я вам покажу
+// як можна це обійти, але поки зробіть так
 
-fs.mkdir(path.join(__dirname, 'main', 'online'), {recursive: true}, (err) => {
+fs.mkdir(path.join(__dirname, 'task1'), (err)=> {
     if (err) {
         console.log(err)
         throw err
     }
 })
 
-fs.mkdir(path.join(__dirname, 'main', 'inPerson'), (err) => {
+fs.writeFile(path.join(__dirname, 'task1', 'file.txt'), '', (err)=> {
     if (err) {
         console.log(err)
         throw err
     }
 })
 
-
-
-fs.readdir(path.join(__dirname, 'main'), (err,files) => {
+fs.appendFile(path.join(__dirname, 'task1', 'file.txt'), 'Hello Okten', (err)=> {
     if (err) {
         console.log(err)
         throw err
     }
-    files.map(dir => fs.writeFile(path.join(__dirname, 'main', dir, `file_${dir}.txt`), '',
-                    (err1) => {
-        if (err1) {
-            console.log(err1)
-            throw err1
-        }
-        console.log('Files created')
-    }))
 })
 
-for (const user of inPersonUser) {
-    for (const userKey in user) {
-    fs.appendFile(path.join(__dirname, 'main', 'inPerson', 'file_inPerson.txt'),
-                            `\n${userKey.toUpperCase()}: ${user[userKey]}`,
-                            (err) => {
-
-        if (err) {
-           console.log(err)
-         throw err
-        }
-        console.log('Content Add')
-    })
+fs.readFile(path.join(__dirname, 'task1', 'file.txt'), "utf8", (err, data)=> {
+    if (err) {
+        console.log(err)
+        throw err
     }
-    fs.appendFile(path.join(__dirname, 'main', 'inPerson', 'file_inPerson.txt'),
-                            `\n===================`,
-                            (err)=> {
+    fs.writeFile(path.join(__dirname, 'task1', 'newFile.txt'), data, (err)=> {
         if (err) {
             console.log(err)
             throw err
         }
-        })
+    })
+})
 
-}
+// ===============================Task2========================================
+// 2. Створіть файл ( можете вручну ) заповніть його якимись даними
+// Прочитайте його, скопіюйте всі дані з нього і перенесіть їх в нову папку та файл в ній,
+// старий файл видаліть після того як все завершиться. Також вийде callback hell
 
+fs.mkdir(path.join(__dirname, 'task2',), (err) => {
+    if (err) {
+        console.log(err)
+        throw err
+    }
+})
 
-for (const user of onlineUser) {
-    for (const userKey in user) {
-        fs.appendFile(path.join(__dirname, 'main', 'online', 'file_online.txt'),
-                        `\n${userKey.toUpperCase()}: ${user[userKey]}`,
-                        (err)=> {
+fs.writeFile(path.join(__dirname, 'task2', 'file.txt'), 'Hello World', (err)=> {
+    if (err) {
+        console.log(err)
+        throw err
+    }
+})
+
+fs.readFile(path.join(__dirname, 'task2', 'file.txt'), "utf8", (err, data)=> {
+    if (err) {
+        console.log(err)
+        throw err
+    }
+    fs.mkdir(path.join(__dirname, 'task2', 'newDir'), (err)=> {
+        if (err) {
+            console.log(err)
+            throw err
+        }
+    })
+    fs.writeFile(path.join(__dirname, 'task2', 'newDir', 'newFile.txt'), data, (err)=> {
+        if (err) {
+            console.log(err)
+            throw err
+        }
+        fs.unlink(path.join(__dirname, 'task2', 'file.txt'), (err) => {
             if (err) {
                 console.log(err)
                 throw err
             }
+        })
+    })
+})
+
+
+// ===============================Task3========================================
+// 3. Створіть папку (можете вручну) напишіть скріпт який створить в ній якись дані
+// (можуть бути нові папки і файли(в файли запишіть якусь дату) )
+// і напишіть функцію яка буде зчитувати папку і перевіряти якщо дані які в ній лежать -
+// це файли тоді вам потрібно їх очистити, але не видаляти, якщо дані - це папки, вам потрібно
+// їх перейменувати і додати до назви префікс _new
+
+fs.mkdir(path.join(__dirname, 'task3'), (err)=> {
+    if (err) {
+        console.log(err)
+        throw err
+    }
+})
+
+for (let i=1; i<4; i++) {
+    fs.mkdir(path.join(__dirname, 'task3', `dir${i}`), (err)=> {
+        if (err) {
+            console.log(err)
+            throw err
+        }
+    })
+    fs.writeFile(path.join(__dirname, 'task3', `file${i}.txt`), `Hello World ${i}`, (err)=> {
+        if (err) {
+            console.log(err)
+            throw err
+        }
+    })
+}
+
+function restoration(directory) {
+    fs.readdir(path.join(__dirname, directory), (err, files)=> {
+        if (err) {
+            console.log(err)
+            throw err
+        }
+        for (const file of files) {
+            fs.stat(path.join(__dirname, directory, file), (err, stats)=> {
+                if (err) {
+                    console.log(err)
+                    throw err
+                }
+
+                if (stats.isDirectory()) {
+                    fs.rename(path.join(__dirname, directory, file), path.join(__dirname, directory, `new_${file}`), (err)=> {
+                        if (err) {
+                            throw err
+                        }
+                    })
+                }
+
+                if (stats.isFile()) {
+                    fs.truncate(path.join(__dirname, directory, file), (err)=> {
+                        if (err) {
+                            throw err
+                        }
+                    })
+                }
             })
-    }
-
-    fs.appendFile(path.join(__dirname, 'main', 'online', 'file_online.txt'),
-                        `\n===============`,
-                        (err)=> {
-
-        if (err) {
-            console.log(err)
-            throw err
         }
     })
 }
-
-
-
-function replace(getFrom, setTo) {
-    fs.readFile(path.join(__dirname, 'main', 'inPerson', getFrom), "utf8",
-                    (err, data)=> {
-        if (err) {
-            console.log(err)
-            throw err
-        }
-
-        fs.appendFile(path.join(__dirname, 'main', 'online', setTo), data, {flag: 'w'},
-                        (err)=> {
-            if (err) {
-                console.log(err)
-                throw err
-            }
-        })
-    })
-
-    fs.readFile(path.join(__dirname, 'main', 'online', setTo), "utf8",
-                        (err, data)=> {
-        if (err) {
-            console.log(err)
-            throw err
-        }
-        fs.appendFile(path.join(__dirname, 'main', 'inPerson', getFrom), data, {flag: 'w'},
-                        (err)=> {
-            if (err) {
-                console.log(err)
-                throw err
-            }
-        })
-    })
-}
-
-// replace('file_inPerson.txt', 'file_online.txt')
+// restoration('task3')
